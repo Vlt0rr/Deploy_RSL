48767357cdcd65e3a6f590fe1c4398d481f259cc
Vlt0rr <vitoralves20112011@gmail.com>
2024-11-05 21:32:08 -0300

    data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -2,13 +2,16 @@ import streamlit as st
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
 
-@st.cache_data
+
+# Estabelecendo a conexão com o Google Sheets
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+# Pegando os dados existentes da planilha/banco
+existing_data = conn.read(worksheet="Dados", usecols=list(range(7)), ttl=5)
+existing_data = existing_data.dropna(how="all")
+
+#@st.cache_data
 def carregar_dados():
-    # Estabelecendo a conexão com o Google Sheets
-    conn = st.connection("gsheets", type=GSheetsConnection)
-    # Pegando os dados existentes da planilha/banco
-    existing_data = conn.read(worksheet="Dados", usecols=list(range(7)), ttl=5)
-    existing_data = existing_data.dropna(how="all")
     tabela = existing_data
     return tabela
 
